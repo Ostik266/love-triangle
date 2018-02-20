5aba2881f65b092bd6c897342c700bd10495261c
Ostik266 <33750170+Ostik266@users.noreply.github.com>
2018-02-20 14:53:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
+  preferences.unshift(0);
+  var x = preferences.length;
+  var count = 0;
+  for (i = 1; i < x; i++)
+  {
+    if ( preferences[preferences[preferences[i]]] === i && preferences[i]!=i)
+    {count++}
+   
+  }
+  return (count/3);
   // your implementation
 };
